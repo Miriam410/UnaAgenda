86dbbb748295b69800f4df9f6c25bd60039400eb
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-05-26 02:01:58 -0300

    Update Persona.cs

--- UnaAgenda/Persona.cs
+++ UnaAgenda/Persona.cs
@@ -8,6 +8,7 @@ namespace UnaAgenda
 {
     public class Persona
     {
+        //Porpiedades de la clase PERSONA
         public int DNI { get; set; }
         public string Apellido { get; set; }
         public string Nombre { get; set; }
@@ -15,9 +16,13 @@ namespace UnaAgenda
         public string Telefono { get; set; }
         public DateTime FechaDeNacimiento { get; set; }
 
+        // El título de entrada solo contiene: Apellido, Nombre - DNI
         public string TituloEntrada => $"{Apellido}, {Nombre} - DNI {DNI}";
 
+        // Es un constructor vacio
         public Persona() {}
+        
+        // Es la linea de datos del archivo .txt
         public Persona (string linea)
         {
             var datos = linea.Split(';');
@@ -29,15 +34,16 @@ namespace UnaAgenda
             FechaDeNacimiento = DateTime.Parse(datos[5]);
         }
 
+        // Se va a obtener la linea de los datos del archivo .txt
         public string ObtenerLineaDatos() => $"{DNI};{Apellido};{Nombre};{Direccion};{Telefono};{FechaDeNacimiento}";
 
+        // Ingresa una nueva PERSONA
         public static Persona IngresarNueva()
         {
+            // Se crea una PERSONA
             var persona = new Persona();
 
             Console.WriteLine("Nueva persona");
-
-
             persona.DNI = IngresarDNI();
             persona.Apellido = Ingreso("Ingrese el apellido");
             persona.Nombre = Ingreso("Ingrese el nombre");
@@ -45,12 +51,13 @@ namespace UnaAgenda
             persona.Telefono = Ingreso("Ingrese el teléfono", permiteNumeros: true);
             persona.FechaDeNacimiento = IngresarFecha("Ingrese la fecha de nacimiento");
 
-
             return persona;
         }
 
+        
         public void Modificar()
         {
+            // Modifica solo el APELLIDO
             Console.WriteLine($"Apellido: {Apellido} - S para modificar / Cualquier tecla para seguir");
             var tecla = Console.ReadKey(intercept: true);
             if(tecla.Key == ConsoleKey.S)
@@ -58,6 +65,7 @@ namespace UnaAgenda
                 Apellido = Ingreso("Ingrese el nuevo apellido");
             }
 
+            // Modifica solo el NOMBRE 
             Console.WriteLine($"Nombre: {Nombre} - S para modificar / Cualquier tecla para seguir");
             tecla = Console.ReadKey(intercept: true);
             if (tecla.Key == ConsoleKey.S)
@@ -65,6 +73,7 @@ namespace UnaAgenda
                 Nombre = Ingreso("Ingrese el nuevo nombre");
             }
 
+            // Modifica solo la DIRECCION 
             Console.WriteLine($"Direccion: {Direccion} - S para modificar / Cualquier tecla para seguir");
             tecla = Console.ReadKey(intercept: true);
             if (tecla.Key == ConsoleKey.S)
@@ -72,6 +81,7 @@ namespace UnaAgenda
                 Direccion = Ingreso("Ingrese la nueva dirección", permiteNumeros: true);
             }
 
+            // Modifica solo el TELEFONO
             Console.WriteLine($"Telefono: {Telefono} - S para modificar / Cualquier tecla para seguir");
             tecla = Console.ReadKey(intercept: true);
             if (tecla.Key == ConsoleKey.S)
@@ -79,6 +89,7 @@ namespace UnaAgenda
                 Telefono = Ingreso("Ingrese el nuevo teléfono", permiteNumeros: true);
             }
 
+            // Modifica solo la FECHA DE NACIMIENTO
             Console.WriteLine($"Fecha de Nacimiento: {FechaDeNacimiento} - S para modificar / Cualquier tecla para seguir");
             tecla = Console.ReadKey(intercept: true);
             if (tecla.Key == ConsoleKey.S)
@@ -86,11 +97,13 @@ namespace UnaAgenda
                 FechaDeNacimiento = IngresarFecha("Ingrese la nueva fecha de nacimiento");
             }
 
+            // Una vez modificado, se va a grabar en el archivo .txt
             Agenda.Grabar();
         }
 
         public void Mostrar()
         {
+            // Se va a mostrar el DNI, APELLIDO, NOMBRE, DIRECCION, TELEFONO, FECHA DE NACIMIENTO(formato: dd/mm/yyyy)
             Console.WriteLine();
             Console.WriteLine($"DNi {DNI}");
             Console.WriteLine($"{Apellido}, {Nombre}");
@@ -102,7 +115,9 @@ namespace UnaAgenda
 
         public static Persona CrearModeloBusqueda()
         {
+            // Se crea un modelo de PERSONA para hacer la busqueda
             var modelo = new Persona();
+            // El modelo tiene todas las propiedades de PERSONA y se va a validar el ingreso de cada una de ellas
             modelo.DNI = IngresarDNI(obligatorio: false);
             modelo.Apellido = Ingreso("Ingrese el apellido", obligatorio: false);
             modelo.Nombre = Ingreso("Ingrese el nombre", obligatorio: false);
@@ -112,8 +127,10 @@ namespace UnaAgenda
             return modelo;
         }
 
+        // VALIDACION INGRESO DE DNI
         private static int IngresarDNI(bool obligatorio = true)
         {
+            // Se pide ingresar un DNI
             var titulo = "Ingrese el DNI (Entero de 8 cifras)";
             if (!obligatorio)
             {
@@ -124,22 +141,24 @@ namespace UnaAgenda
             {
                 Console.WriteLine(titulo);
                 var ingreso = Console.ReadLine();
+                // si no pide ingresar dni y es un string vacio o nulo, devuelve 0
                 if (!obligatorio && string.IsNullOrWhiteSpace(ingreso))
                 {
                     return 0;
                 }
-
+                // si no ingresa un dni válido
                 if (!int.TryParse(ingreso, out var dni))
                 {
                     Console.WriteLine("No ha ingresado un DNI válido");
                     continue;
                 }
-
+                // no es válido si ingresa un dni menos de 10000000 o mas de 99999999
                 if (dni < 10_000_000 || dni > 99_999_999)
                 {
                     Console.WriteLine("Debe ser un número de 8 cifras");
                     continue;
                 }
+                // si en la agenda ya existe el dni
                 if (Agenda.Existe(dni))
                 {
                     Console.WriteLine("El DNI indicado ya existe en agenda");
@@ -153,6 +172,8 @@ namespace UnaAgenda
 
         public bool CoincideCon(Persona modelo)
         {
+            // IF SON TODOS LOS CASOS QUE NO COINCIDEN EL ORIGINAL CON EL MODELO
+            // si el dni el modelo de Persona  es diferente a 0 y es diferente al dni persona(original)
             if (modelo.DNI != 0 && modelo.DNI != DNI)
             {
                 return false;
@@ -181,6 +202,7 @@ namespace UnaAgenda
 
         }
 
+        // VALIDACION FECHA
         private static DateTime IngresarFecha(string titulo, bool obligatorio = true)
         {
             do
@@ -213,6 +235,8 @@ namespace UnaAgenda
 
             } while (true);
         }
+        
+        // VALIDACION DE TIPO STRING(APELLIDO, NOMBRE, DIRECCION, TELEFONO)
         private static string Ingreso(string titulo, bool permiteNumeros = false, bool obligatorio = true)
         {
             string ingreso;
