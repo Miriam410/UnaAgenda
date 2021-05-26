15d170f65f48c4305d3de66342492c7e4325874e
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-05-26 02:07:22 -0300

    Update Agenda.cs

--- UnaAgenda/Agenda.cs
+++ UnaAgenda/Agenda.cs
@@ -20,43 +20,54 @@ namespace UnaAgenda
             //Streamwriter => escribir archivos
 
             entradas = new Dictionary<int, Persona>();
-
+            // se pregunta si existe el nombre de archivo que ingresó el usuario por consola
             if (File.Exists(nombreArchivo))
             {
+                // si existe el archivo, va a leer el archivo
                 using (var reader = new StreamReader(nombreArchivo))
                 {
                     while (!reader.EndOfStream)
                     {
+                        // se va a leer por linea
                         var linea = reader.ReadLine();
+                        // se crea una persona con parámetros de la linea(son las propiedades de persona parseada)
                         var persona = new Persona(linea);
+                        // se agrega la persona el dni y al objeto persona
                         entradas.Add(persona.DNI, persona);
                     }
                 }
 
             }
         }
+        
+        // Agregar una PERSONA
         public static void Agregar(Persona persona)
         {
             entradas.Add(persona.DNI, persona);
             Grabar();
         }
 
+        // Dar de baja una PERSONA
         public static void Baja(Persona persona)
         {
             entradas.Remove(persona.DNI);
             Grabar();
         }
 
+        // Preguntar si existe el DNI
         public static bool Existe(int dni)
         {
             return entradas.ContainsKey(dni);
         }
 
+        // Seleccionar la persona comparandolo con un modelo
         public static Persona Seleccionar()
         {
+            // se crea un modelo de busqueda
             var modelo = Persona.CrearModeloBusqueda();
             foreach (var persona in entradas.Values)
             {
+                // si la persona coincide con el modelo
                 if (persona.CoincideCon(modelo))
                 {
                     return persona;
@@ -67,8 +78,10 @@ namespace UnaAgenda
             return null;
         }
 
+        // Se va a grabar en un archivo .txt
         public static void Grabar()
         {
+            // se va a escribir el archivo, pide el nombre del archivo a escribir 
             using (var writer = new StreamWriter(nombreArchivo, append: false))
             {
                 foreach (var persona in entradas.Values)
